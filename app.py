40e0687f6e60a9853bfba579b939571149af5c0b
GabyruFlores <sgabyflor9@gmail.com>
2026-09-23 21:04:29 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
+import pandas as pd
 st.title("Bolsa de Valores Quito BI")
 st.sidebar.title("Parámetros")
 st.write("Elaborado por: Gabriela Flores")
 
 archivo = st.file_uploader("Cargue su archivo")
+tabla = pd.read:csv(archivo)
+st.write(tabla)
